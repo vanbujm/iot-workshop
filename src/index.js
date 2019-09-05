eae479bd4161e8baeaaaf858f4f380cc9ff6fb00
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-05 11:34:42 +0800

    Turned flash code into a web server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('test configuration');
+/* eslint-disable no-console */
+import express from 'express';
+import { init } from 'raspi';
+import { LED, } from 'raspi-led';
+import { flashLED, turnOffLED } from './flashService';
+
+const parseSpeed = (speed) => {
+  const speedNum = Number(speed);
+  if (Number.isNaN(speedNum)) return null;
+  return speedNum;
+};
+
+let ledInterval;
+
+init(() => {
+  const statusLed = new LED();
+
+  const app = express();
+  const port = 3000;
+
+  app.get('/', ({ query: { speed } }, res) => {
+    const requestedSpeed = parseSpeed(speed);
+
+    if (!requestedSpeed && ledInterval) {
+      clearInterval(ledInterval);
+      turnOffLED(statusLed);
+      return res.send('Off');
+    }
+
+    const flashSpeed = requestedSpeed || 500;
+
+    if (ledInterval) clearInterval(ledInterval);
+
+    ledInterval = setInterval(() => flashLED(statusLed), flashSpeed);
+
+    return res.send(`Flashing at ${flashSpeed}ms`);
+  });
+
+  app.listen(port, () => console.info(`App listening on port ${port}!`));
+});
