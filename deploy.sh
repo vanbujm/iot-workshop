b78127cf3aa5be2fcf7a3806da6a54ce999f8652
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-06 10:03:48 +0800

    Adding CircleCi

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+deploy() {
+  docker run -v "$(pwd)":/usr/app -v "$(yarn cache dir)":/yarn-cache vanbujm/purple-iot-worshop-balena:latest -c "yarn --cache-folder  /yarn-cache && yarn && yarn build"
+  cd build || exit 1
+  cp ../images/prod/* ./
+  eval "$(ssh-agent -s)"
+  echo -e ${BALENA_CLOUD_KEY} >id_rsa
+  chmod 0600 id_rsa
+  ssh-add ./id_rsa
+  ssh-keyscan git.balena-cloud.com >>balenakey
+  cat balenakey >>~/.ssh/known_hosts
+  git remote add balena ${BALENA_REMOTE}
+  git fetch --unshallow origin
+  git commit -am "build" --allow-empty
+  git push -f balena master
+}
+
+deploy
