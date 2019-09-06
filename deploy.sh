7a96e84d1e097dc6bb5840d9a19add8fb54524aa
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-06 11:01:29 +0800

    Fixing travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 deploy() {
-#  docker run -v "$(pwd)":/usr/app -v "$(yarn cache dir)":/yarn-cache vanbujm/purple-iot-worshop-balena:latest bash -c "yarn --cache-folder  /yarn-cache && yarn && yarn build"
-  docker run -v "$(pwd)":/usr/app -v "$(yarn cache dir)":/yarn-cache vanbujm/purple-iot-worshop-balena:latest bash -c "which yarn && which node"
+  docker run -v "$(pwd)":/usr/app -v "$(yarn cache dir)":/yarn-cache vanbujm/purple-iot-worshop-balena:latest bash -c "yarn --cache-folder  /yarn-cache && yarn && yarn build"
   cd build || exit 1
   cp ../images/prod/* ./
   eval "$(ssh-agent -s)"
