c3d125c68ca5492c10bf029bd26db7e77277a001
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-09 11:50:00 +0800

    fixing travis

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,17 @@
 deploy() {
   eval "$(ssh-agent -s)" \
-  && echo -e "${BALENA_CLOUD_KEY}" >id_rsa \
-  && chmod 0600 id_rsa \
-  && ssh-add ./id_rsa \
-  && ssh-keyscan git.balena-cloud.com >>balenakey \
-  && cat balenakey >>~/.ssh/known_hosts \
-  && git remote add balena "${BALENA_REMOTE}" \
-  && git fetch balena \
-  && ls -a \
-  && cp ./images/prod/Dockerfile ./ \
-  && git status \
-  && git add -f Dockerfile \
-  && git commit -m "Travis build: ${TRAVIS_BUILD_ID}" --allow-empty \
-  && git status \
-  && echo "Pushing to balena 😎" \
-  && git push -f balena master:balena-nocache
+  echo -e "${BALENA_CLOUD_KEY}" >id_rsa
+  chmod 0600 id_rsa
+  ssh-add ./id_rsa
+  ssh-keyscan git.balena-cloud.com >>balenakey
+  cat balenakey >>~/.ssh/known_hosts
+  rm -rdf .git
+  git init
+  git remote add balena "${BALENA_REMOTE}"
+  cp ./images/prod/Dockerfile ./
+  git commit -am "Travis build: ${TRAVIS_BUILD_ID}" --allow-empty
+  echo "Pushing to balena 😎"
+  git push -f balena master:balena-nocache
 }
 
 deploy
