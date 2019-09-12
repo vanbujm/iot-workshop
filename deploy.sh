dc8196c57a8244b439d1ce7a7d3615ffeaed55a8
Jonathan Van Buren <vanbujm@gmail.com>
2019-09-12 14:24:41 +0800

    Revert "Trying to simplify build"
    This reverts commit c7d475d82e2d58bc4569aaeb79152a3a16b38b9e.

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ deploy() {
   echo -e "${BALENA_CLOUD_KEY}" >id_rsa
   chmod 0600 id_rsa
   ssh-add ./id_rsa
-#  ssh-keyscan git.balena-cloud.com >>balenakey
-#  cat balenakey >>~/.ssh/known_hosts
+  ssh-keyscan git.balena-cloud.com >>balenakey
+  cat balenakey >>~/.ssh/known_hosts
   rm -rdf .git
   git init
   git remote add balena "${BALENA_REMOTE}"
